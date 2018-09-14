639c987ab99ae54a41d6d0a40c8f5f2eba177ba6
Adriano Faria Alves <adriano.faria@outlook.com.br>
2018-09-14 20:52:56 -0300

    Criado o serviço de empresas e teste básico

--- src/main/kotlin/com/siswbrasil/pontointeligente/repositories/LancamentoRepository.kt
+++ src/main/kotlin/com/siswbrasil/pontointeligente/repositories/LancamentoRepository.kt
@@ -2,8 +2,8 @@ package com.siswbrasil.pontointeligente.repositories
 
 import com.siswbrasil.pontointeligente.documents.Lancamento
 import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
-import java.awt.print.Pageable
 
 interface LancamentoRepository : MongoRepository<Lancamento, String> {
 
